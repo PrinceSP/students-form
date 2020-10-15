e1c355cdd6ac145db2ab0eaef1ed43662b97f61d
PrinceSP <30646160+PrinceSP@users.noreply.github.com>
2020-10-15 19:56:20 +0800

    add filter function and add new row-datas into table

--- src/app.js
+++ src/app.js
@@ -1,25 +1,35 @@
 const form = document.querySelector('.form');
 const gender = document.querySelectorAll('input[type="radio"]');
-const id = document.querySelector('input[name="id"]').value;
-const fname = document.querySelector('input[name="fullname"]').value;
-const email = document.querySelector('input[name="email"]').value;
-const fac = document.querySelector('[name="fil-faculty"]');
-const prog = document.querySelector('[name="fil-programs"]');
 const show = document.querySelector('[name="button-show"]');
+const table = document.querySelector('table');
+const main = document.querySelector('#faculties');
+const pos = document.querySelector('#programs');
+
+const programs = {
+  theology:['bechelor theology','magister of theology'],
+  nursing:['bachelor nursing','proffession'],
+  agriculture:['agriculture'],
+  comp:['system information','information of technology'],
+  feb:['accounting','management','internatinal program'],
+  sec:['secretary'],
+  edu:['religious education','english education','economic education','non-formal education']
+}
+
 
 //set form display to none
 form.style.display = 'none';
 
-
+//header fixed when window scrolled
 window.addEventListener('scroll',function(){
   let head = document.querySelector('header');
   head.classList.toggle('fixed',window.scrollY > 0);
 })
 
+
 //hide-show button for information form
 show.addEventListener('click',function(e){
   e.stopPropagation();
-  if (form.style.display === "none") {
+  if (form.style.display == "none") {
     form.style.display = 'block';
     this.textContent = 'Hide Form';
   } else {
@@ -30,42 +40,44 @@ show.addEventListener('click',function(e){
 
 form.addEventListener('submit',(e)=>{
   e.preventDefault();
-
-  //get ID
+  const id = document.querySelector('input[name="id"]').value;
+  const fname = document.querySelector('input[name="fullname"]').value;
+  const email = document.querySelector('input[name="email"]').value;
+  const th = document.querySelectorAll('th');
   console.log(id);
-  //get fullname
   console.log(fname);
-
-  //get email
   console.log(email);
-
-  //get gender value
   gender.forEach((itm,idx,arr)=>{
     if (arr[idx].checked) {
       console.log(arr[idx].value);
     }
-  })
+  });
+
+  const rows = document.createElement('tr');
+  const tdId = document.createElement('td');
+  const tdFname = document.createElement('td');
+  const tdGender = document.createElement('td');
+
+  tdId.textContent = id;
+  tdFname.textContent = fname;
+  tdGender.textContent = gender;
+
+  rows.appendChild(tdId);
+  rows.appendChild(tdFname);
+  rows.appendChild(tdGender);
+  
+  table.appendChild(rows);
 });
 
-fac.addEventListener('click',(e)=>{
-  e.preventDefault();
-  const facList = fac.childNodes;
-  facList.forEach(item=>{
-    switch (e.target.value) {
-      case 'all':
-        break;
-      case 'theology':
-        console.log('theology');
-        break;
-      case 'nursing':
-        console.log('nursing');
-        break;
-      case 'agriculture':
-        console.log('agriculture');
-        break;
-      case 'comp':
-        console.log('computer science');
-        break;
-    }
+main.addEventListener('change',function(e){
+  const selected = programs[this.value];
+
+  while (pos.options.length>0) {
+    pos.options.remove(0);
+  }
+
+  Array.from(selected).forEach(el=>{
+    const option = new Option(el,el);
+    pos.appendChild(option);
   })
 });
